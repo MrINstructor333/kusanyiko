2d9e29ba130a63fc0d376f44bd1d2d0968485e9f
Your Name <Your Name>
2025-10-05 11:22:47 +0300

    Switch to HashRouter to fix page reload 404 errors

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useAppSelector } from '../hooks/redux';
 
 // Auth Pages
